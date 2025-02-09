9069f47783aa808f1aa90870a2288f7d69d61916
Lucas <lucas.dasilvafaria@hotmail.com>
2025-02-09 16:45:59 -0300

    project

---
+++ src/app/components/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+
+    return (
+        <nav>
+            <ul className="flex justify-center gap-4 upp font-semibold p">
+                <li className="p-3">
+                    Sobre mim
+                </li>
+                <li className="p-3">
+                    Formação
+                </li>
+                <li className="p-3">
+                    Portfólio
+                </li>
+                <li className="p-3">
+                    Contato
+                </li>
+            </ul>
+        </nav>
+    )
+
+}
